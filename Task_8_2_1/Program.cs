4daccc18c21769cdb63befb694bafc8e5223120c
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-24 22:20:58 +0300

    master: task 8.2.1

---
+++ Task_8_2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                var rootDir = drive.RootDirectory;
+                var files = rootDir.GetFiles();
+                var folders = rootDir.GetDirectories();
+                Console.WriteLine($"На диске {drive.Name} расположено:{Environment.NewLine}\t{files.Length} файлов{Environment.NewLine}\t{folders.Length} директорий");
+            }
+        }
+    }
+}
