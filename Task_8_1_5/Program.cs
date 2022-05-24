c34d78b590069bc6f4ff4a420c22a4ce24c68ed3
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-24 19:46:51 +0300

    master: add task 8.1.5

---
+++ Task_8_1_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_8_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <string, Folder> Folders = new Dictionary<string, Folder>();
+            NewFolder(Folders, "Dir1");
+        }
+    public static void NewFolder (Dictionary<string, Folder> DictDirs, string nameDir)
+        {
+            DictDirs.Add(nameDir, new Folder());
+            Console.WriteLine($"made \"{nameDir}\" directory");
+        }       
+    }
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+        
+    }
+}
