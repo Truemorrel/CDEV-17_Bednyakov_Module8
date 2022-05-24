bbc098131251d2eef80b8005921531425299de7c
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-25 01:59:13 +0300

    master: add tasks 8.2.3, 8.2.4

---
+++ Task_8_2_3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                var rootDir = drive.RootDirectory;
+                var files = rootDir.GetFiles();
+                var folders = rootDir.GetDirectories();
+                Console.WriteLine($"На диске {drive.Name} расположено:{Environment.NewLine}\t{files.Length} файлов{Environment.NewLine}\t{folders.Length} директорий");
+            }
+            xFilesIn();
+            foreach (DriveInfo drive in drives)
+            {
+                var rootDir = drive.RootDirectory;
+                var files = rootDir.GetFiles();
+                var folders = rootDir.GetDirectories();
+                Console.WriteLine($"На диске {drive.Name} расположено:{Environment.NewLine}\t{files.Length} файлов{Environment.NewLine}\t{folders.Length} директорий");
+            }
+            xFilesOut();
+
+        }
+        public static void xFilesIn()
+        {
+            try
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                foreach (DriveInfo drive in drives)
+                {
+                    DirectoryInfo rootDir = new DirectoryInfo(string.Concat(drive.Name, @"xDir"));
+                    if (!rootDir.Exists)
+                    {
+                        rootDir.Create();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void xFilesOut()
+        {
+            try
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo drive in drives)
+                {
+                    DirectoryInfo rootDir = new DirectoryInfo(string.Concat(drive.Name, @"xDir"));
+                    if (rootDir.Exists)
+                    {
+                        rootDir.Delete(true);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
---
+++ Task_8_2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo testDir = new DirectoryInfo(@"C:\Users\Desktop\testFolder");
+            if (testDir.Exists)
+            {
+                testDir.MoveTo(@"C:\$Recycle.bin");
+            }
+        }
+    }
+}
