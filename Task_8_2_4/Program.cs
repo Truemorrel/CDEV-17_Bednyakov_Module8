d1db39a59d94eb1346f88852f5c069678c1411f7
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-25 01:59:13 +0300

    master: update 8.2.4

---
+++ Task_8_2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                DirectoryInfo testDir = new DirectoryInfo(@"C:\Users\Sergey\Desktop\testFolder");
+                if (testDir.Exists)
+                {
+                    testDir.MoveTo(@"C:\$Recycle.bin\testFolder");
+                 //   testDir.MoveTo(@"C:\Users\Sergey\.Trash\testFolder");
+                }
+            }
+            catch(Exception e)
+            { Console.WriteLine(e.Message); }
+        }
+    }
+}
