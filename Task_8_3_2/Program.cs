cda6a0e4c910e13d7a27b9f5b0720b539e7a00d1
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-25 17:32:40 +0300

    master: add task 8.3.2

--- Task_8_3_2/Program.cs
+++ Task_8_3_2/Program.cs
@@ -1,27 +1,33 @@
 ﻿using System;
 using System.IO;
 
-namespace Task_8_3_1
+namespace Task_8_3_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string sfile = @"d:\Repos\CDEV-17_Bednyakov_Module8\Task_8_3_1\Program.cs";
-            if(File.Exists(sfile))
+            string sfile = @"d:\Repos\CDEV-17_Bednyakov_Module8\Task_8_3_2\Program.cs";
+            if (File.Exists(sfile))
             {
                 using (StreamReader sr = File.OpenText(sfile))
                 {
                     //Console.WriteLine(sr.ReadToEnd());
                     string textLine = sr.ReadLine();
-                    while(textLine != null)
+                    while (textLine != null)
                     {
                         Console.WriteLine(textLine);
                         textLine = sr.ReadLine();
                     }
                 }
+                FileInfo fi = new FileInfo(sfile);
+                using (StreamWriter sw = fi.AppendText())
+                {
+                    sw.WriteLine("//{0}", DateTime.Now);
+                };
+
             }
         }
     }
 }
-////25.05.2022 17:30:43
+//25.05.2022 17:31:37
