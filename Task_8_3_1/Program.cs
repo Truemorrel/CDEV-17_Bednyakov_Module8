f0e1b7c3c0330c683607608e9d99749b34b82efb
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-25 14:15:10 +0300

    master: update task 8.3.1

---
+++ Task_8_3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sfile = @"d:\Repos\CDEV-17_Bednyakov_Module8\Task_8_3_1\Program.cs";
+            if(File.Exists(sfile))
+            {
+                using (StreamReader sr = File.OpenText(sfile))
+                {
+                    //Console.WriteLine(sr.ReadToEnd());
+                    string textLine = sr.ReadLine();
+                    while(textLine != null)
+                    {
+                        Console.WriteLine(textLine);
+                        textLine = sr.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
