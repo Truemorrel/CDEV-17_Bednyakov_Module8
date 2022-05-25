71bf1eb2621a821cd7469bb4abb27d33057ffb46
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-25 21:51:33 +0300

    mater: task 8.4.1

---
+++ Task_8_4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fn = @"C:\Users\Sergey\Desktop\BinaryFile.bin";
+            var fi = new FileInfo(fn);
+            ;
+            if (File.Exists(fn))
+            {
+                using (BinaryReader sf = new BinaryReader(File.Open(fn, FileMode.Open)))
+                {
+                    Console.WriteLine($"{sf.ReadString()}");
+
+                }
+            }
+        }
+    }
+}
