5b01bdc4529cc2983bcf825b108cd2acffa40f84
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-27 17:41:48 +0300

    master: add task1

--- Task_8_4_2/Program.cs
+++ Task_8_4_2/Program.cs
@@ -10,11 +10,6 @@ namespace Task_8_4_2
             string fn = @"C:\Users\Sergey\Desktop\BinaryFile.bin";
             if (File.Exists(fn))
             {
-                using (BinaryReader sf = new BinaryReader(File.Open(fn, FileMode.Open)))
-                {
-                    Console.WriteLine($"{sf.ReadString()}");
-
-                }
                 using (BinaryWriter sf = new BinaryWriter(File.Open(fn, FileMode.Append)))
                 {
                     string day = string.Concat((((DateTime.Today).Day < 10) ? "0" : ""), (DateTime.Today).Day);
@@ -22,6 +17,12 @@ namespace Task_8_4_2
                     string hour = string.Concat((((DateTime.Now).Hour < 10) ? "0" : ""), (DateTime.Now).Hour);
                     string minute = string.Concat((((DateTime.Now).Minute < 10) ? "0" : ""), (DateTime.Now).Minute);
                     sf.Write($"{Environment.NewLine}Файл изменен {day}.{month} {hour}:{minute} на компьютере c ОС {Environment.OSVersion}");
+                }
+                
+                using (BinaryReader sf = new BinaryReader(File.Open(fn, FileMode.Open)))
+                {
+                    Console.WriteLine($"{sf.ReadString()}");
+
                 }
             }
         }
---
+++ Task_8_4_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Task_8_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string personFile = @"C:\Users\Sergey\Desktop\contactSerialize.dat";
+            var person = new Contact("Piter", +123456789, "piter@person.com");
+            Console.WriteLine("Создан контакт: {0}", person.Name);
+            BinaryFormatter formater = new BinaryFormatter();
+            using FileStream fs = new FileStream(personFile, FileMode.OpenOrCreate);
+            {
+                formater.Serialize(fs, person);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+    }
+    [Serializable]
+    class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
